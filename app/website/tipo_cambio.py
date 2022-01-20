cf4ef61ead678f02e9ab98715e1aa94bdc305c64
AFRV21 <97257839+AFRV21@users.noreply.github.com>
2022-01-19 23:08:27 -0600

    Agregando función
    Conversión de dolares a quetzales, archivo tipo_cambio.py

--- app/website/tipo_cambio.py
+++ app/website/tipo_cambio.py
@@ -6,3 +6,11 @@ def tipo_cambio_dia():
     tipo_de_cambio_dia = float(cliente.service.TipoCambioDia()['CambioDolar']['VarDolar'][0]['referencia'])
     fecha = cliente.service.TipoCambioDia()['CambioDolar']['VarDolar'][0]['fecha']
     return tipo_de_cambio_dia, fecha
+
+def convertir_usd_a_gtq(usd):
+    cliente = Client('https://www.banguat.gob.gt/variables/ws/TipoCambio.asmx?WSDL')
+    dolares = float(usd)
+    tc, fecha = tipo_cambio_dia()
+    quetzales = dolares*tc
+    texto = str(usd) + " USD -> " + str(quetzales) + " GTQ" + " calculado el " + str(fecha)
+    return texto
