3b5b26491997a525caaf3bbce73f97ba58b3c32d
AFRV21 <97257839+AFRV21@users.noreply.github.com>
2022-01-20 18:27:20 -0600

    Corrigiendo decimales e impresión
    Modificación en la salida del cálculo de conversión, *pendiente terminar presentación.*

--- app/website/tipo_cambio.py
+++ app/website/tipo_cambio.py
@@ -8,15 +8,15 @@ def tipo_cambio_dia():
     return tipo_de_cambio_dia, fecha
 
 def convertir_usd_a_gtq(usd):
-    dolares = float(usd)
+    dolares = float(round(usd,2))
     tc, fecha = tipo_cambio_dia()
-    quetzales = dolares*tc
-    texto = str(usd) + " USD -> " + str(quetzales) + " GTQ" + " calculado el " + str(fecha)
+    quetzales = round((dolares*tc),2)
+    texto = str("{:.2f}".format(usd)) + " USD -> " + str(quetzales) + " GTQ" + " calculado el " + str(fecha)
     return texto
 
 def convertir_gtq_a_usd(gtq):
-    quetzales = float(gtq)
+    quetzales = float(round(gtq,2))
     tc, fecha = tipo_cambio_dia()
-    dolares = quetzales/tc
-    texto = str(gtq) + " GTQ -> " + str(dolares) + " USD" + " calculado el " + str(fecha)
+    dolares = round((quetzales/tc),2)
+    texto = str("{:.2f}".format(gtq)) + " GTQ -> " + str(dolares) + " USD" + " calculado el " + str(fecha)
     return texto
